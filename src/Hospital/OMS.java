2c340f199a236bed142b488da318144ef297437f
KimberlyZuniga07 <48140909+KimberlyZuniga07@users.noreply.github.com>
2020-06-03 16:32:21 -0600

    Abstracta OMS continuacion

--- src/Hospital/OMS.java
+++ src/Hospital/OMS.java
@@ -7,25 +7,46 @@ package Hospital;
 
 import Paciente.Paciente;
 import Prueba.Covid19Prueba;
+import Prueba.PCRPrueba;
+import Prueba.PruebaRapida;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author Kimbe
+ * @author KimberlyZuniga
  */
 public abstract class OMS {
-    protected abstract Hospital getNombreHospital();
-    protected abstract boolean isGAMHospital();
-    
-    private final List<Covid19Prueba> resultList = new ArrayList<Covid19Prueba>(); 
-    
-    public OMS(List<Paciente>pacientee){//Constructor con lista de obj paciente
-   
+
+    protected abstract Hospital getNombreHospital();//funcion abstracta de tipo hospital
+
+    protected abstract boolean isGAMHospital(); //funcion abstracta protegida
+
+    private final List<Covid19Prueba> resultList = new ArrayList<Covid19Prueba>();
+
+    public OMS(List<Paciente> pacientee) {//Constructor con lista de obj paciente
+
         diagnosticar(pacientee);
     }
 
+    //
     private void diagnosticar(List<Paciente> pacientee) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Paciente paciente : pacientee) {
+            if (isGAMHospital()) {
+                resultList.add(new PCRPrueba(paciente));
+            } else {
+                resultList.add((Covid19Prueba) new PruebaRapida(paciente));
+            }
+        }
+    }
+
+    public final void printReport() {
+        System.out.println("Resultado del hospital" + getNombreHospital().getNombre());
+        for (Covid19Prueba resultado : resultList) {
+            System.out.println("-----------");
+            System.out.println("nombre = " + resultado.getNombrePaciente());
+            System.out.println("es caso positivo = " + resultado.isPositiveCase());
+
+        }
     }
 }
