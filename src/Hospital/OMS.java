a0a41819b4e79ab421524a6f6f2abcfe1af835b7
KimberlyZuniga07 <48140909+KimberlyZuniga07@users.noreply.github.com>
2020-06-03 16:21:59 -0600

    Abstracta OMS

--- src/Hospital/OMS.java
+++ src/Hospital/OMS.java
@@ -5,10 +5,27 @@
  */
 package Hospital;
 
+import Paciente.Paciente;
+import Prueba.Covid19Prueba;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Kimbe
  */
-public class OMS {
+public abstract class OMS {
+    protected abstract Hospital getNombreHospital();
+    protected abstract boolean isGAMHospital();
     
+    private final List<Covid19Prueba> resultList = new ArrayList<Covid19Prueba>(); 
+    
+    public OMS(List<Paciente>pacientee){//Constructor con lista de obj paciente
+   
+        diagnosticar(pacientee);
+    }
+
+    private void diagnosticar(List<Paciente> pacientee) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/Hospital/TestExamenUno.java
+++ src/Hospital/TestExamenUno.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package examenuno;
+package Hospital;
 
 /**
  *
  * @author Kimbe
  */
-public class ExamenUno {
+public class TestExamenUno {
 
     /**
      * @param args the command line arguments
