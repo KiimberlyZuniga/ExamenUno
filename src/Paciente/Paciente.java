2bd4bf41278362872051253659305502c2483b54
KimberlyZuniga07 <48140909+KimberlyZuniga07@users.noreply.github.com>
2020-06-02 22:29:57 -0600

    Modificación de clase Paciente con respectivos atributos inmutables, constructor, y getters

--- src/Paciente/Paciente.java
+++ src/Paciente/Paciente.java
@@ -7,8 +7,43 @@ package Paciente;
 
 /**
  *
- * @author Kimbe
+ * @author KimberlyZuniga
  */
 public class Paciente {
+    //ATRIBUTOS INMUTABLES 
+    private final String nombrePaciente;
+    private final int edadPaciente;
+    private final int temperaturaPaciente;
+    private final boolean vomito;
+
+    //CONST
+    public Paciente(String nombrePaciente, int edadPaciente, int temperaturaPaciente, boolean vomito) {
+        this.nombrePaciente = nombrePaciente;
+        this.edadPaciente = edadPaciente;
+        this.temperaturaPaciente = temperaturaPaciente;
+        this.vomito = vomito;
+    }
+
+    //FALTA INV COMO HACER LOS ATRIBUTOS INMUTABLES
     
+    //SOLO GETTER PARA QUE NO PUEDAN SER MODIFICADOS
+    public String getNombrePaciente() {
+        return nombrePaciente;
+    }
+
+    public int getEdadPaciente() {
+        return edadPaciente;
+    }
+
+    public int getTemperaturaPaciente() {
+        return temperaturaPaciente;
+    }
+
+    public boolean isVomito() {
+        return vomito;
+    }
+
+    public boolean vomito() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
