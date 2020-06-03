993c46044458811156b7dd94cc7b9ccdad2006b2
KimberlyZuniga07 <48140909+KimberlyZuniga07@users.noreply.github.com>
2020-06-03 10:19:07 -0600

    Clase INMUTABLE PCR Prueba (metodo)

--- src/Prueba/PCRPrueba.java
+++ src/Prueba/PCRPrueba.java
@@ -5,6 +5,8 @@
  */
 package Prueba;
 
+import Paciente.Paciente;
+
 /**
  *
  * @author KimberlyZuniga
@@ -12,7 +14,15 @@ package Prueba;
 
 //CLASE INMUTABLE (FINAL)
 public  final class PCRPrueba implements Covid19Prueba{
+    private boolean isPositive = false;
+    private final Paciente paciente;
 
+    //Const de obj tipo paciente
+    public PCRPrueba(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    
     @Override
     public boolean isPositiveCase() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -21,6 +31,12 @@ public  final class PCRPrueba implements Covid19Prueba{
     @Override
     public String getNombrePaciente() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    //metodo da positivo si la temp es = o > a 39
+      private void ejecutarPrueba() {
+        isPositive = paciente.getTemperaturaPaciente()>= 39 && paciente.vomito();
     }
     
 }
